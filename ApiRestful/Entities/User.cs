8887e4f02ab6d5c7a09ce24be7e8d195ee9af053
Jair Manosalva <yayomanosalva@gmail.com>
2024-09-18 17:18:52 -0500

    Agregar archivos de proyecto.

---
+++ ApiRestful/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiRestful.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public ICollection<WishlistItem> WishlistItems { get; set; }
+    }
+}
---
+++ ApiRestful/Entities/WishlistItem.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiRestful.Entities
+{
+    public class WishlistItem
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+        public User User { get; set; }
+    }
+}
